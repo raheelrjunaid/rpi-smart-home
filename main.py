bd43eaf310b3d8ef4555e8a336d1f488d18b299f
Raheel Junaid <raheelj2004@gmail.com>
2021-01-23 21:15:43 -0700

    Add fan test code

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import keypad
+from gpiozero import DigitalOutputDevice
+# import keypad
+from time import sleep
 from signal import pause
 
+fan = DigitalOutputDevice(17)
+
 try:
-    keypad.main()
+    # keypad.main()
+    while True:
+        fan.on()
+        sleep(1)
     pause()
 except KeyboardInterrupt:
-    keypad.close()
+    # keypad.close()
     print('\nExited Program')
