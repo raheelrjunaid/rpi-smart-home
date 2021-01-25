13c84a5c99915e96b032aa1187fd4a0ba27dfe34
Raheel Junaid <raheelj2004@gmail.com>
2021-01-24 22:16:07 -0700

    Add initial lock position for servo

--- global_vars.py
+++ global_vars.py
@@ -1,4 +1,4 @@
-from gpiozero import Servo, Buzzer
+from gpiozero import AngularServo, Buzzer
 
-servo = Servo(18)
+servo = AngularServo(18, 90, min_pulse_width=0.16/1000, max_pulse_width=2/1000)
 buzzer = Buzzer(4)
