e89300097f1d9db86ff0a1d10e559b0c7789d440
Raheel Junaid <raheelj2004@gmail.com>
2021-01-24 23:12:58 -0700

    Add LCD componentent + Systems display

--- global_vars.py
+++ global_vars.py
@@ -1,4 +1,11 @@
 from gpiozero import AngularServo, Buzzer
+from rpi_lcd import LCD
 
 servo = AngularServo(18, 90, min_pulse_width=0.16/1000, max_pulse_width=2/1000)
+screen = LCD()
+systems = {
+    'fan': 1,
+    'servo': 1,
+    'keypad': 1,
+}
 buzzer = Buzzer(4)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from global_vars import servo
+from global_vars import screen, systems
 from gpiozero import DigitalOutputDevice, LED, RGBLED, Servo
 from colorzero import Color
 import keypad
@@ -8,11 +8,30 @@ from signal import pause
 fan = DigitalOutputDevice(17)
 RGBLed = RGBLED(27, 23, 25)
 
+def readSystems(line):
+    cat_string = ''
+    if line == 1:
+        for system in systems:
+            cat_string += system[0].upper()
+    else:
+        for system in systems:
+            status = systems[system]
+            if status == 1:
+                cat_string += 'T' # True
+            else:
+                cat_string += 'F' # False
+    return cat_string
+
+
 try:
     keypad.main()
     fan.on()
+    screen.text(f'Systems {(len(systems) + 1) * "-"} ' + readSystems(1), 1)
+    screen.text(f'Status {(len(systems) + 2) * "-"} ' + readSystems(2), 2)
+    print(systems)
     pause()
 except KeyboardInterrupt:
     # FIX(Maybe) Module has trouble cleaning up with this function but the main function still works
     # keypad.close()
+    screen.clear()
     print('\nExited Program')
