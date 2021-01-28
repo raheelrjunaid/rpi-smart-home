d5f6e92a96fc620952e9b9b8c0337b6ef25fed06
Raheel Junaid <raheelj2004@gmail.com>
2021-01-28 16:37:42 -0700

    Change Timer while loop to for loop + add comments

--- door_system.py
+++ door_system.py
@@ -17,28 +17,25 @@ servo = Servo(18, 1)
 buzzer = Buzzer(17)
 
 def triggerTimer():
-    count = 0
-
-    while True:
-
+    count = 0 # Current countdown in seconds
+    for count in range(11):
+        
+        # Interrupt countdown if signal is recieved from keypad (main.py)
         if timerStateInput.value == 1:
             print('Thank you. Stopping timer process on next read.')
             break
         
-        if count <= 10:
-            print(("You have " + str(10 - count) + " seconds remaining\r"), end="")
-            count += 1
-            buzzer.beep(0.1, 0.1, n=1)
-            time.sleep(1)
-
-        else:
-            break
+        # Beep every 1 second
+        print(("You have " + str(10 - count) + " seconds remaining\r"), end="")
+        buzzer.beep(0.1, 0.1, n=1)
+        time.sleep(1)
     
+    # Only execute this code if the for loop has been completed
     if count >= 10:
         print('Contact System Administrator to Disable')
         servo.max()
         buzzer.beep()
-        pause()
+        pause() # Countinuously beep forever
 
 timer_thread = Process(target=triggerTimer)
 authUserIDs = [248227650093]
@@ -47,26 +44,35 @@ log = []
 try:
     while True:
 
+        # Reset timer on next read
         if timerStateInput.value == 1:
             print('Timer Stopped')
-            log = []
+            log = [] # Reset log so duplicate entry is allowed
             timerStateInput.value = 0
             timer_thread.terminate()
 
+        # Reset authentication status
         authenticated = False
+
+        # Read data from RFID Reader 
+        # This is why the while loop only executes as this function waits for data
         id, text = rfid.read()
         log.append(id)
 
+        # Ensure the list remains only 2 entries long
         if len(log) > 2:
             log.pop(0)
 
+        # Only execute if it is not a duplicate reading or if there is only 1 reading
         if (len(log) > 1 and log[-1] != log[-2]) or len(log) == 1:
             buzzer.beep(0.2, 0.2, n=1)
 
+            # Compare all stored authenticated user id's to the present user ID
             for userID in authUserIDs:
                 if id == userID:
                     authenticated = True
 
+            # Only execute if the alarm hasn't been tripped
             if not timer_thread.is_alive():
                 if authenticated == True:
                     servo.min() # Open Door (servo)
@@ -78,7 +84,8 @@ try:
                     print('Authentication Error')
         
 except KeyboardInterrupt:
-    log = []
-    servo.max()
+    log = [] # Reset Log
+    servo.max() # Close the door
     GPIO.cleanup() # For RFID reader only
+
     print('\nExited Loop')
