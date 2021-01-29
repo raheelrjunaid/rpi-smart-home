d3eb3abdc79b73c59eca285202a2b5067b56deb1
Raheel Junaid <raheelj2004@gmail.com>
2021-01-28 21:41:08 -0700

    Add Recording function

--- camera_pir.py
+++ camera_pir.py
@@ -1,9 +1,31 @@
-from gpiozero import DistanceSensor
+from gpiozero import MotionSensor
+from datetime import datetime
+from global_vars import camLED
+from picamera import PiCamera
+from multiprocessing import Process
 from time import sleep
+
+pirMotionSensor = MotionSensor(5)
+camera = PiCamera(resolution=(1280, 720))
+
+def newRecording():
+    camera.start_recording("./cam_videos/" + datetime.now().strftime('%m.%d.%Y-%H:%M:%S') + ".h264")
+    print('Recording')
+    camera.wait_recording(10)
+    camera.stop_recording()
+    print('Finished Recording')
 
 # Standard test program, not to be used for main functionality — refer to main file (main.py)
 try:
+
     while True:
-        sleep(1)
+        
+        if pirMotionSensor.value == 1:
+            newRecording()
+
+        # Represents recording duration sensitivity
+        sleep(2)
+
 except KeyboardInterrupt:
+    camera.close()
     print('\nExited Program')
