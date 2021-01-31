908592c3033c0d83a0e46827c80892a9e7647613
Raheel Junaid <raheelj2004@gmail.com>
2021-01-30 20:55:38 -0700

    Control remote components and threads directly, add thread monitoring, Added a developer mode which outputs the running threads when requested. Properly closed components directly after joining all threads concurrently with messages. Messages correspond to newly added Thread names.

--- main.py
+++ main.py
@@ -2,27 +2,61 @@
 # Author: Raheel Junaid
 # Date Started: 1/20/21
 
-from global_vars import disarmSystem, showSystemStatus, armSystem, screen
+from global_vars import disarmSystem, showSystemStatus, armSystem, screen, remote_buzzer, fan
 import keypad, camera_pir, os, furnace_adc
-from threading import Thread
-from colorzero import Color
-from time import sleep
+from threading import Thread, enumerate
+from time import sleep, time
 from signal import pause
 
+def monitorThreads():
+    while True:
+        print("-" * 20)
+        for thread in enumerate():
+            print(thread.name)
+        print("-" * 20)
+        sleep(3)
+
+threads = [
+    Thread(name="ADC", target=furnace_adc.main, daemon=True),
+    Thread(name="KeypadTimeout", target=keypad.startTimer, daemon=True),
+    Thread(name="Camera", target=camera_pir.main, daemon=True)
+]
+
 try:
-    keypad.main() # "keypad" Required to avoid circular import
-    # furnace_adc.main()
+    keypad.keypad.registerKeyPressHandler(keypad.tryKey)
+    
+    for thread in threads:
+        thread.start()
+
+    if os.environ['DEVMODE'] == "True":
+        Thread(name="Monitoring", target=monitorThreads, daemon=True).start()
+
     armSystem()
     showSystemStatus()
-    camera_pir.main()
     pause()
 except KeyboardInterrupt:
-    # FIX(Maybe) Module has trouble cleaning up with this function but the main function still works
-    # keypad.close()
-
-    camera_pir.camera.close()
-    # furnace_adc.leds.close()
     print('\nShutting Down Systems...')
+    # Show timer
+    startTime = time()
+
+    for thread in threads:
+        # Allow threads to end processess
+        print(("-" * 20) + "\nWaiting for " + thread.name + "\n" + ("-" * 20), (len(threads) * 10) - int(time() - startTime) + "seconds remaining")
+        # Warn user of closing exceptions
+        if threads.index(thread) == len(threads) - 1:
+            print('On the last thread, Error may return')
+        thread.join(5)
+
+    # Close down all optional systems
     screen.clear()
+
+    # Remote systems need to be closed instead of turned off
+    remote_buzzer.close()
+    fan.close()
+
+    # Close down all mandatory systems
+    furnace_adc.leds.close()
+    camera_pir.camera.close()
     disarmSystem()
-    print('Exited Program')
+
+    print('\nExited Program')
